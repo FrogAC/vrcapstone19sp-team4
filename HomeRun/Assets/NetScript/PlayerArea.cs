53f9b0bb263cf408c4f0faee054d73310595347e
daoyee <acgfrog@outlook.com>
2019-05-06 05:29:31 -0700

    update 2019.1; migrate net code bases

---
+++ HomeRun/Assets/NetScript/PlayerArea.cs
@@ -0,0 +1,56 @@
+namespace HomeRun.Net
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+	using Oculus.Platform.Models;
+
+	public class PlayerArea : MonoBehaviour
+	{
+		// the prefab for the ball that players will shoot
+		[SerializeField] private GameObject m_ballPrefab = null;
+
+		// cached gameobject that where the player camera will move to
+		private GameObject m_playerHead;
+
+		// cached Text component where we'll render the player's name
+		private Text m_nameText;
+
+		public Player Player
+		{
+			get { return m_playerHead.GetComponent<Player>(); }
+		}
+
+		public Text NameText
+		{
+			get { return m_nameText; }
+		}
+
+		void Awake()
+		{
+			m_playerHead = gameObject.transform.Find("Player Head").gameObject;
+			m_nameText = gameObject.GetComponentsInChildren<Text>()[1];
+		}
+
+		public T SetupForPlayer<T>(string name) where T : Player
+		{
+			var oldplayer = m_playerHead.GetComponent<Player>();
+			if (oldplayer) Destroy(oldplayer);
+
+			var player = m_playerHead.AddComponent<T>();
+			player.BallPrefab = m_ballPrefab;
+			m_nameText.text = name;
+
+			if (player is RemotePlayer)
+			{
+			}
+			else if (player is LocalPlayer)
+			{
+			}
+			else
+			{
+			}
+
+			return player;
+		}
+	}
+}
---
+++ HomeRun/Assets/NetScript/RemotePlayer.cs
@@ -0,0 +1,35 @@
+namespace HomeRun.Net
+{
+	using Oculus.Platform.Models;
+
+	public class RemotePlayer : Player
+	{
+		private User m_user;
+
+		public User User
+		{
+			set { m_user = value; }
+		}
+
+		public ulong ID
+		{
+			get { return m_user.ID; }
+		}
+
+		public override uint Score
+		{
+			set
+			{
+				// For now we ignore the score determined from locally scoring backets.
+				// To get an indication of how close the physics simulations were between devices,
+				// or whether the remote player was cheating, an estimate of the score could be
+				// kept and compared against what the remote player was sending us.
+			}
+		}
+
+		public void ReceiveRemoteScore(uint score)
+		{
+			base.Score = score;
+		}
+	}
+}
