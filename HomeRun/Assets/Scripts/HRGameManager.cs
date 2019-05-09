747ffe4c08a2a87ac1bd0d480c61dee41b83e640
andrewrudasics <43798507+andrewrudasics@users.noreply.github.com>
2019-05-09 11:46:48 -0700

    Updated Scoreboard

---
+++ HomeRun/Assets/Scripts/HRGameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HRGameManager : MonoBehaviour
+{
+    public delegate void HomeRunDelegate();
+    public event HomeRunDelegate homeRunEvent;
+
+    private int homeruns;
+
+
+    public void HomeRun()
+    {
+        if (homeRunEvent != null)
+        {
+            homeRunEvent();
+        }
+    }
+}
---
+++ HomeRun/Assets/Scripts/ScoreboardManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreboardManager : MonoBehaviour
+{
+    private Canvas scoreboard;
+    private Text awayText, homeText, inningText;
+    private int homeScore, awayScore, inning, balls, strikes, outs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    public void IncrementInning()
+    {
+        SetInning(inning + 1);
+    }
+
+    public void SetInning(int inning)
+    {
+        this.inning = inning;
+        inningText.text = this.inning.ToString();
+    }
+
+    public void IncrementHomeScore()
+    {
+        homeScore++;
+        UpdateHomeScore();
+    }
+
+    public void SetAwayNull()
+    {
+        awayText.text = "--";
+    }
+
+    public void IncrementAwayScore()
+    {
+        awayScore++;
+        UpdateAwayScore();
+    }
+
+    public void UpdateAwayScore()
+    {
+        if (awayScore < 10)
+        {
+            awayText.text = "0";
+        }
+        awayText.text += awayScore.ToString();
+    }
+
+    public void UpdateHomeScore()
+    {
+        if (homeScore < 10)
+        {
+            homeText.text = "0";
+        }
+        homeText.text += homeScore.ToString();
+    }
+
+    public int GetInning() { return inning; }
+    public int GetHomeScore() { return homeScore; }
+    public int GetAwayScore() { return awayScore; }
+    public int GetBalls() { return balls; }
+    public int GetStrikes() { return strikes; }
+    public int GetOuts() { return strikes; }
+}
