200d5c35bad74b62bdb54ddd06c743af0201a8d4
TStrong85 <acgfrog@outlook.com>
2019-05-08 21:03:52 -0700

    remove useless states/msgs

--- HomeRun/Assets/NetScript/RemotePlayer.cs
+++ HomeRun/Assets/NetScript/RemotePlayer.cs
@@ -16,20 +16,5 @@ namespace HomeRun.Net
 			get { return m_user.ID; }
 		}
 
-		public override uint Score
-		{
-			set
-			{
-				// For now we ignore the score determined from locally scoring backets.
-				// To get an indication of how close the physics simulations were between devices,
-				// or whether the remote player was cheating, an estimate of the score could be
-				// kept and compared against what the remote player was sending us.
-			}
-		}
-
-		public void ReceiveRemoteScore(uint score)
-		{
-			base.Score = score;
-		}
 	}
 }
