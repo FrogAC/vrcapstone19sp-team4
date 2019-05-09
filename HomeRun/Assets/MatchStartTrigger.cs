be84893c77dc5cd33c46e0150e95d1c59125f410
TStrong85 <acgfrog@outlook.com>
2019-05-08 21:03:52 -0700

    remove useless states/msgs

---
+++ HomeRun/Assets/MatchStartTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HomeRun.Net;
+
+public class MatchStartTrigger : MonoBehaviour
+{
+    [SerializeField] private MatchController matchController;
+    void OnTriggerEnter(Collider other) {
+        // Based on Type?
+        matchController.PlayOnlineOrCancel();
+    }
+}
