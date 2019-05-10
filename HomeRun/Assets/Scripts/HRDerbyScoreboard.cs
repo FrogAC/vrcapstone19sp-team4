a69ba91b0b9788dfc9e7d823d15778a123b85e2a
andrewrudasics <43798507+andrewrudasics@users.noreply.github.com>
2019-05-10 01:42:48 -0700

    Updated Scoreboards

---
+++ HomeRun/Assets/Scripts/HRDerbyScoreboard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HRDerbyScoreboard : MonoBehaviour
+{
+    public Text timerText, scoreText;
+
+    void Start()
+    {
+        UpdateScoreText(0);
+        UpdateTimeText(60);
+    }
+
+    public void UpdateTimeText(int time)
+    {
+        int sec = time % 60;
+        int min = time / 60;
+        string res = (min >= 10) ? "" + min : ("0" + min);
+        res += ":" + ((sec >= 10) ? "" + sec : ("0" + sec));
+        timerText.text = res;
+    }
+
+    public void UpdateScoreText(int score)
+    {
+        string res = "";
+        if (score < 10)
+        {
+            res += "0";
+        }
+        res += score;
+        scoreText.text = res;
+    }
+}
--- HomeRun/Assets/Scripts/HRGameManager.cs
+++ HomeRun/Assets/Scripts/HRGameManager.cs
@@ -7,15 +7,143 @@ public class HRGameManager : MonoBehaviour
 {
     public delegate void HomeRunDelegate();
     public event HomeRunDelegate homeRunEvent;
+    public delegate void OnTimeUp();
+    public event OnTimeUp TimeUpEvent;
+
+    public HRDerbyScoreboard scoreboard;
+    public int timeAllowed;
 
     private int homeruns;
+    private bool isPaused;
+    private Coroutine timer;
 
+    private void Start()
+    {
+        // Init Variables
+        if (timeAllowed <= 0)
+            timeAllowed = 60;
+        isPaused = false;
+        TimeUpEvent += GameOver;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PauseGame();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ResumeGame();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetTimer();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartTimer();
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            HomeRun();
+        }
+    }
 
     public void HomeRun()
     {
+        homeruns++;
+        scoreboard.UpdateScoreText(homeruns);
         if (homeRunEvent != null)
         {
             homeRunEvent();
         }
     }
+
+    public void GameOver()
+    {
+        TimeUpEvent -= GameOver;
+        Debug.Log("GameOver");
+    }
+
+    
+
+    //====================================================
+    // Game Flow Methods
+    //====================================================
+
+    public void StartGame()
+    {
+
+    }
+
+    public void PauseGame()
+    {
+        if (!isPaused)
+            isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        if (isPaused)
+            isPaused = false;
+    }
+
+    public void ResetGame()
+    {
+        
+    }
+
+    //====================================================
+    // Timer Methods
+    //====================================================
+
+    public void StartTimer()
+    {
+        ResetTimer();
+        timer = StartCoroutine(Timer(timeAllowed));
+        TimeUpEvent += GameOver;
+    }
+
+    public void ResetTimer()
+    {
+        if (timer != null)
+            StopCoroutine(timer);
+
+        isPaused = false;
+        scoreboard.UpdateTimeText(timeAllowed);
+        
+    }
+
+    // Coroutine Runs Timer
+    IEnumerator Timer(int time)
+    {
+        int i = time;
+        while (i >= 0)
+        {
+            while (isPaused)
+            {
+                yield return null;
+            }
+            scoreboard.UpdateTimeText(i);
+            if (i == 0)
+            {
+                // TimeUpEven
+                if (TimeUpEvent != null)
+                {
+                    TimeUpEvent();
+                }
+            }
+            i -= 1;
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    //====================================================
+    // Accessors
+    //====================================================
 }
