8b812def8fd06f51d85b2ea234765330333cc434
TStrong85 <33169441+TStrong85@users.noreply.github.com>
2019-05-15 21:12:44 -0700

    Zombies Grenade implemented, zombies prototyped

---
+++ HomeRun/Assets/MoveTowards.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowards : MonoBehaviour
+{
+    public Vector3 targetPosition;
+    public float moveSpeed = 1;
+    public float stopDistance = 3;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position, targetPosition) > stopDistance)
+        {
+            Vector3 moveDir = targetPosition - transform.position;
+            transform.position += moveDir.normalized * moveSpeed * Time.fixedDeltaTime;
+        }
+    }
+}
