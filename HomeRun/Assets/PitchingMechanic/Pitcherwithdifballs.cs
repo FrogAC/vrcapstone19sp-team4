4ca61198bbbc30a76dafc9778a4365aa231fd6f3
TStrong85 <acgfrog@outlook.com>
2019-05-28 02:19:33 -0700

    improve routine in ThrownBall to avoid Start() call conflit

--- HomeRun/Assets/PitchingMechanic/Pitcherwithdifballs.cs
+++ HomeRun/Assets/PitchingMechanic/Pitcherwithdifballs.cs
@@ -53,16 +53,12 @@ public class Pitcherwithdifballs : MonoBehaviour
     {
         baseballPrefab = ballToThrow[currIndex];
         ThrownBall ball = Instantiate(baseballPrefab, transform.position, transform.rotation);
-        int typei = currIndex == 2 ? currIndex : 1-currIndex;
-        ball.balltype = (BallType)typei;
 
         if (GlobalSettings.UseNetwork && PlatformManager.CurrentState == PlatformManager.State.PLAYING_A_NETWORKED_MATCH)
         {
-            PlatformManager.P2P.AddNetworkBall(ball.gameObject, (BallType)typei);
+            PlatformManager.P2P.AddNetworkBall(ball.gameObject, ball.balltype);
         }
-        ball.initialize();
         ball.GrabEnd(transform.forward * speed, Vector3.zero);
-        //Destroy(ball, lifetime);
     }
 
     // Update is called once per frame
