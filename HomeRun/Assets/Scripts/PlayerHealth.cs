d0d4e2da35f54d108368a96dd1635eadc9d72d1d
TStrong85 <33169441+TStrong85@users.noreply.github.com>
2019-05-29 23:29:23 -0700

    Updated Zombie Explosions delay and added a player health field

---
+++ HomeRun/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Zombie"))
+        {
+            health -= 1;
+            if (health <= 0) Debug.Log("Player Just Died");
+        }
+    }
+}
