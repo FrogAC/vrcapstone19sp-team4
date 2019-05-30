fe4620f23ff65cbbae2acfda6388c3bd9ce9287d
TStrong85 <33169441+TStrong85@users.noreply.github.com>
2019-05-29 22:11:03 -0700

    Added Event System

---
+++ HomeRun/Assets/_Plugins/EventSystem/scripts/EventListener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class EventListener: MonoBehaviour {
+
+	public GameEvent Event;
+	public UnityEvent Response;
+
+	//register and deregister by enabling
+	void OnEnable () {Event.Register (this);}
+	void OnDisable () {Event.DeRegister (this);}
+
+	public void OnEventRaised(){Response.Invoke();Debug.Log (this.name);}
+		
+}
