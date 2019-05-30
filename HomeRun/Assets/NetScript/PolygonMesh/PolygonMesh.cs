1351b4e45082ae23500293db04c8b0c1b54ce269
TStrong85 <acgfrog@outlook.com>
2019-05-29 22:06:39 -0700

    have fun on filed

---
+++ HomeRun/Assets/NetScript/PolygonMesh/PolygonMesh.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+using UnityEditor;
+
+// TODO: ugly toggle
+// TODO: allow disable up/down faces
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+public class PolygonMesh : MonoBehaviour {
+    [Header("Enable Editing")]
+    public bool enableEdit = false;
+    [HideInInspector]
+    [SerializeField]
+    public List<Vector3> points = new List<Vector3>();
+    public float y = 0f;
+    public bool showMesh = false;
+    public bool enableCollidor = false;
+    public bool enableHeight = false;
+    [ConditionalHide("enableHeight", true)]
+    public float height = 1.0f;
+    public Material material;
+
+    MeshRenderer meshRenderer;
+    [HideInInspector]
+    public MeshFilter meshFilter;
+    MeshCollider meshCollider;
+    Vector3[] localVerts;
+    int[] tris;
+
+    [HideInInspector]
+    [SerializeField]
+    public List<Action> updateMeshPerfromed = new List<Action>();
+    
+    public float localY {
+        get {
+            return transform.InverseTransformPoint(new Vector3(0, y, 0)).y;
+        }
+    }
+
+    void Start() {
+        ToggleMeshRender();
+        UpdatePointsPosition();
+    }
+
+    void OnValidate() {
+        if (meshFilter == null) meshFilter = gameObject.GetComponent<MeshFilter>();
+        // y or height
+        UpdatePointsPosition();
+    }
+
+    public void ToggleMeshRender() {
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        if (!showMesh) {
+            if (meshRenderer != null) DestroyImmediate(meshRenderer);
+            meshRenderer = null;
+        } else {
+            if (meshRenderer == null) {
+                gameObject.AddComponent<MeshRenderer>();
+                meshRenderer = gameObject.GetComponent<MeshRenderer>();
+            }
+            meshRenderer.material = material;
+        }
+    }
+
+    public void ToggleMeshCollider() {
+        meshCollider = gameObject.GetComponent<MeshCollider>();
+        if (!enableCollidor) {
+            if (meshCollider != null) DestroyImmediate(meshCollider);
+            meshCollider = null;
+        } else {
+            if (meshCollider == null) {
+                gameObject.AddComponent<MeshCollider>();
+                meshCollider = gameObject.GetComponent<MeshCollider>();
+            }
+            UpdatePointsPosition();
+        }
+    }
+
+    public void UpdateMesh() {
+
+        localVerts = points.Select(p => transform.InverseTransformPoint(p)).ToArray();
+        Vector2[] points2 = localVerts.Select(p => new Vector2(p.x, p.z)).ToArray();
+        tris = new Triangulator(points2).Triangulate();
+        // LOCAL POSITION
+        transform.position = points.First();
+
+        if (enableHeight) {
+            int count = points.Count;
+            localVerts = localVerts.Concat(localVerts).ToArray();
+            var trisList = tris.Concat(tris.Select(i => i + count)).ToList();
+            for (int i = 0; i < count; i++) {
+                trisList.Add(i + count);
+                trisList.Add((i + 1) % count);
+                trisList.Add(i);
+
+                trisList.Add(((i + 1) % count + count) % (2 * count));
+                trisList.Add((i + 1) % count);
+                trisList.Add(i + count);
+            }
+            tris = trisList.ToArray();
+        }
+        UpdatePointsPosition();
+        // ACTION LIST
+        foreach (Action a in updateMeshPerfromed) a();
+    }
+
+    void UpdatePointsPosition() {
+        points = points.Select(p => new Vector3(p.x, y, p.z)).ToList();
+
+        if (localVerts == null || localVerts.Length == 0) return;
+        for (int i = 0; i < points.Count; i++) {
+            localVerts[i] = new Vector3(localVerts[i].x, localY, localVerts[i].z);
+        }
+        if (enableHeight && localVerts.Length == points.Count * 2) {
+            for (int i = points.Count; i < points.Count * 2; i++) {
+                localVerts[i] = new Vector3(localVerts[i].x, localY + height, localVerts[i].z);
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = localVerts;
+        mesh.triangles = tris;
+        mesh.RecalculateNormals();
+        meshFilter.mesh = mesh;
+        if (meshCollider != null) meshCollider.sharedMesh = mesh;
+    }
+}
---
+++ HomeRun/Assets/NetScript/SimpleRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour
+{
+    [SerializeField] private float m_speed = 1.0f;
+    IEnumerator spin()
+    {
+        while (true)
+        {
+            transform.Rotate(0, m_speed * Time.deltaTime, 0);
+            yield return null;
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(spin());
+    }
+}
