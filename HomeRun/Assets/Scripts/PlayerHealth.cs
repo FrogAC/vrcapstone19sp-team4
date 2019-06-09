ab12841499225a1945af82760b2d3893a2ed1ef0
TStrong85 <33169441+TStrong85@users.noreply.github.com>
2019-06-09 16:12:32 -0700

    zombie stuff

--- HomeRun/Assets/Scripts/PlayerHealth.cs
+++ HomeRun/Assets/Scripts/PlayerHealth.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public int health;
+    public Color safeColor;
+    public Color notSafeColor;
+    public GameObject hitNotifier;
+
     // Start is called before the first frame update
+    private void Start()
+    {
+        safeColor = hitNotifier.GetComponent<Renderer>().material.color;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag.Equals("Zombie"))
         {
             Debug.Log("Zombie Trigger Occured");
             health -= 1;
+            changeColor();
             if (health <= 0) Debug.Log("Player Just Died");
         }
     }
+
+    IEnumerator changeColor()
+    {
+        hitNotifier.GetComponent<Renderer>().material.color = Color.Lerp(safeColor, notSafeColor, 5.0f);
+        yield return null;
+       // hitNotifier.GetComponent<Renderer>().material.color = Color.Lerp(notSafeColor, safeColor, 1.0f);
+    }
+
+
 }
